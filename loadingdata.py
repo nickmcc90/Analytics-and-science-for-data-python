3a54d9a5723944516347c743d25811b984e33d1f
Nicholas McCarroll <nickmccarroll8@gmail.com>
2024-08-08 17:22:22 -0400

    doing sql queries

---
+++ loadingdata.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+import sqlite3
+
+# reading csv file
+data_csv = pd.read_csv('filename.csv')
+
+# reading txt file
+data_txt = pd.read_txt('textfile', header = 0, sep = ',')
+
+# reading excel file
+data_excel = pd.read_excel('textfile', header = 0, sep = ',')
+
+# reading json file
+data_json = pd.read_json('textfile', header = 0, sep = ',')
+
+# connection to sql database
+connection_db = sqlite3.connect("database_name.db")
+query_1 = 'SELECT col_1 FROM table_name'
